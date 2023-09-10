dc566ae824a4edabee78a519c591894f848444d8
hermer29 <kliman.chan@gmail.com>
2023-09-10 23:51:32 +0300

    Initial commit

---
+++ src/CheatMenuDevelopment/Assets/Playtesting/PlaytestingCanvas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Hermer29.Cheats;
+using Hermer29.Cheats.DebugValues;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Playtesting
+{
+    public class PlaytestingCanvas : MonoBehaviour, ICheatHandler
+    {
+        [SerializeField] private Button _createCheats;
+        [SerializeField] private InputField _key;
+        [SerializeField] private InputField _value;
+        [SerializeField] private Button _submit;
+
+        private void Start()
+        {
+            _createCheats.onClick.AddListener(OnCheatsCreateRequested);
+            _submit.onClick.AddListener(OnKeyValueSubmit);
+        }
+
+        private void OnKeyValueSubmit()
+        {
+            Cockpit.SetReadOnly(_key.text, _value.text);
+        }
+
+        private void OnCheatsCreateRequested()
+        {
+            Cheats.Create(new ICheatHandler[] { this});
+        }
+
+        public string GetCheatCode()
+        {
+            return "myCheat";
+        }
+
+        public void Execute()
+        {
+            Debug.Log("My cheat called");
+        }
+    }
+}
